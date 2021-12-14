fc64f296a79c70493f83cf031e129cd872ff6484
MarianoPd <marianopidone@gmail.com>
2021-12-14 15:26:34 +0100

    secion 4 in corso

--- src/components/Section4Plans.vue
+++ src/components/Section4Plans.vue
@@ -1,14 +1,50 @@
 <template>
   <div class="container-fluid my-container">
       <div class="container my-subcontainer">
-          
+          <div class="my-head-text white">
+              <h2>
+                  Our Plans
+              </h2>
+              <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit assumenda fugit rem optio tenetur officiis sapiente maiores perferendis!
+              </p>
+          </div>
+          <div class="row">
+                <div v-for="(card, index) in cardList" :key="index"
+                    class="col-3 ">
+                    <div class="my-card">
+                        <i :class="card.iconClass"></i>
+                        <h3>{{card.mainText}}</h3>
+                        
+                        <span>{{card.perks[0]}} Projects</span>
+                        <span>{{card.perks[0]}} Storage</span>
+                        <span>{{card.perks[0]}} Users</span>
+                        
+                    </div>
+                </div>
+          </div>
       </div>
   </div>
 </template>
 
 <script>
+import db from '@/assets/data/db.json'
 export default {
     name: 'Section4Plans',
+    data(){
+        return{
+            cardList: [],
+        }
+    },
+    methods:{
+        getList(type){
+            let array = db.cards.filter( card => card.type == type);
+            return array;
+        }
+    },
+    mounted(){
+        this.cardList = this.getList('plans');
+    }
 }
 </script>
 
@@ -21,6 +57,18 @@ export default {
     @include background-set();
     .my-subcontainer{
         height: 840px; //da togliere
+        .my-card{
+            background-color: white;
+            h3{
+                padding: 20px;
+            }
+            span{
+                color: $primary-gray;  
+                display: block;  
+                margin-bottom: 40px;
+            }
+            
+        }
     }
 }
 
